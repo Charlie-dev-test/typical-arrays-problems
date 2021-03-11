485e2a2895c6bbde50a1c56c6bfcff4590f8ad14
Charlie-dev-test <mailbox.boyko@gmail.com>
2021-03-11 18:46:30 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!array){
+        return 0;
+    }
+    if(array.length == 0){
+        return 0;
+    }
+  return Math.min(...array, 0);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array){
+        return 0;
+    }
+    if(array.length == 0){
+        return 0;
+    }
+  return Math.max(...array, 0);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array){
+        return 0;
+    }
+    if(array.length == 0){
+        return 0;
+    }
+    let summary = 0;
+    for(let i=0; i<array.length; i++){
+        summary = summary + array[i]
+    }
+
+  return summary/array.length;
 }
